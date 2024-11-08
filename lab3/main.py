4b9d647fc3685b7e17e1824f6388830b3514513a
AndreyPreobrazhenskiy <164298315+AndreyPreobrazhenskiy@users.noreply.github.com>
2024-11-08 21:36:16 +0400

    Update main.py

--- lab3/main.py
+++ lab3/main.py
@@ -1,4 +1,5 @@
-from hist import make_hist
+from hist import create_hist
+from hist import display_hist
 from image_funcs import display_image
 from image_funcs import load_image
 from image_funcs import resize_image
@@ -7,13 +8,20 @@ from parser import parse_arguments
 
 
 def main():
-    args = parse_arguments()
-    image = load_image(args.input_path)
-    make_hist(image)
-    display_image("Исходное изображение", image)
-    resized_image = resize_image(image, args.width, args.height)
-    display_image("Измененное изображение", resized_image)
-    save_image(args.output_path, resized_image)
+    try:
+        args = parse_arguments()
+        image = load_image(args.input_path)
+
+        hist_b, hist_g, hist_r = create_hist(image)
+        display_hist(hist_b, hist_g, hist_r)
+
+        display_image("Исходное изображение", image)
+        resized_image = resize_image(image, args.width, args.height)
+        display_image("Измененное изображение", resized_image)
+
+        save_image(args.output_path, resized_image)
+    except (FileNotFoundError, ValueError) as e:
+        print(e)
 
 
 if __name__ == "__main__":
