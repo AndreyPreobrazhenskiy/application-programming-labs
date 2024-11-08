5cbb6de1232d99725d63f3275480c05f9f99d555
AndreyPreobrazhenskiy <164298315+AndreyPreobrazhenskiy@users.noreply.github.com>
2024-11-08 21:35:10 +0400

    Update image_funcs.py

--- lab3/image_funcs.py
+++ lab3/image_funcs.py
@@ -1,22 +1,26 @@
 import cv2
 import matplotlib.pyplot as plt
+import numpy as np
 
 
-def load_image(input_path):
+def load_image(input_path: str) -> np.ndarray:
     """
     Loads the image in the specified path.
 
     :param input_path: Path to the input image file.
     :return: The loaded image as a NumPy array.
     """
-    image = cv2.imread(input_path)
-    if image is None:
-        raise FileNotFoundError(f"Ошибка: не удалось загрузить изображение по пути '{input_path}'.")
-    print("Размер исходного изображения:", image.shape)
-    return image
+    try:
+        image = cv2.imread(input_path)
+        if image is None:
+            raise FileNotFoundError(f"Ошибка: не удалось загрузить изображение в: '{input_path}'.")
+        return image
+    except Exception as e:
+        print(f"Не удалось загрузить изображение: {e}")
+        raise
 
 
-def display_image(title, image):
+def display_image(title: str, image: np.ndarray) -> None:
     """
     Displays an image with a given title.
 
@@ -24,14 +28,17 @@ def display_image(title, image):
     :param image: numpy.ndarray - The image to be displayed.
     :return: None
     """
-    plt.figure(figsize=(8, 8))
-    plt.title(title)
-    plt.axis("off")
-    plt.imshow(cv2.cvtColor(image, cv2.COLOR_BGR2RGB))
-    plt.show()
+    try:
+        plt.figure(figsize=(8, 8))
+        plt.title(title)
+        plt.axis("off")
+        plt.imshow(cv2.cvtColor(image, cv2.COLOR_BGR2RGB))
+        plt.show()
+    except Exception as e:
+        print(f"Не удалось вывести изображение  '{title}': {e}")
 
 
-def resize_image(image, width, height):
+def resize_image(image: np.ndarray, width: int, height: int) -> np.ndarray:
     """
     Resizes an image to the specified width and height.
 
@@ -40,14 +47,25 @@ def resize_image(image, width, height):
     :param height: New height of the image.
     :return: The resized image.
     """
-    return cv2.resize(image, (width, height))
+    try:
+        resized_image = cv2.resize(image, (width, height))
+        return resized_image
+    except Exception as e:
+        print(f"Не удалось поменять разрешение изображение на {width}x{height}: {e}")
+        raise
 
 
-def save_image(output_path, image):
+def save_image(output_path: str, image: np.ndarray) -> None:
     """
     Saves the image to the specified output path.
 
     :param output_path: Path to save the output image.
     :param image: The image to save.
     """
-    cv2.imwrite(output_path, image)
+    try:
+        success = cv2.imwrite(output_path, image)
+        if not success:
+            raise IOError(f"Не удалось сохранить изображение в '{output_path}'.")
+    except Exception as e:
+        print(f"Не удалось сохранить изображение: {e}")
+        raise
