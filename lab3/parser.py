939916216e6b5f0a0c684bced5f38d00988b3f8f
AndreyPreobrazhenskiy <164298315+AndreyPreobrazhenskiy@users.noreply.github.com>
2024-11-08 22:59:46 +0400

    Update parser.py

--- lab3/parser.py
+++ lab3/parser.py
@@ -15,15 +15,11 @@ def parse_arguments() -> argparse.Namespace:
     parser.add_argument('height', type=int, help="Новая высота изображения.")
     args = parser.parse_args()
 
-    try:
-        args = parser.parse_args()
-        if not os.path.isfile(args.input_path):
-            raise FileNotFoundError(f"Ошибка: файл '{args.input_path}' не найден.")
+    args = parser.parse_args()
+    if not os.path.isfile(args.input_path):
+        raise FileNotFoundError(f"Ошибка: файл '{args.input_path}' не найден.")
 
-        if args.width <= 0 or args.height <= 0:
-            raise ValueError("Ошибка: параметры width и height должны быть целыми положительными числами.")
+    if args.width <= 0 or args.height <= 0:
+        raise ValueError("Ошибка: параметры width и height должны быть целыми положительными числами.")
 
-        return args
-    except Exception as e:
-        print(f"Не удалось запарсить аргументы: {e}")
-        raise
+    return args
